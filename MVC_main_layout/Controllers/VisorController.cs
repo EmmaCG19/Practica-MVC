181839745d964a421ea110b67b3ad5bc23d6bea5
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-02-26 20:54:39 -0300

    Added template for an histogram UI

---
+++ MVC_main_layout/Controllers/VisorController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVC_main_layout.Models;
+
+namespace MVC_main_layout.Controllers
+{
+    public class VisorController : Controller
+    {
+        [HttpGet]
+        public ActionResult Histograma()
+        {
+            VotosVM model = new VotosVM();
+            return View(model);
+        }
+    }
+}
---
+++ MVC_main_layout/Models/VotosVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_main_layout.Models
+{
+    public class VotosVM
+    {
+        public VotosVM()
+        {
+            this.CandidatoA = 30;
+            this.CandidatoB = 40;
+            this.CandidatoC = 30;
+        }
+
+        public int CandidatoA { get; set; }
+        public int CandidatoB { get; set; }
+        public int CandidatoC { get; set; }
+
+    }
+}
