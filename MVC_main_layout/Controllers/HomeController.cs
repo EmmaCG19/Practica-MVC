b5301ad3bbe83f0e7ae3a6dc239b50f9a2945cbe
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-02-26 20:16:49 -0300

    Added project in which Bootstrap library is tested and different views display the same model.

--- MVC_main_layout/Controllers/HomeController.cs
+++ MVC_main_layout/Controllers/HomeController.cs
@@ -3,15 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_main_layout.Models;
 
 namespace MVC_main_layout.Controllers
 {
     public class HomeController : Controller
     {
-        // GET: Home
-        public ActionResult Index()
+        //Mostrar el mismo ViewModel de dos maneras diferentes
+
+        UsuarioVM model = new UsuarioVM();
+
+        [HttpGet]
+        public ActionResult Listar()
         {
-            return View();
+            return View(model);
+        }
+
+        [HttpGet]
+        public ActionResult ListarDistinto()
+        {
+            return View(model);
         }
     }
 }
---
+++ MVC_main_layout/Models/UsuarioVM.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using MVC_main_layout.DAL;
+
+namespace MVC_main_layout.Models
+{
+    public class UsuarioVM
+    {
+        public List<Usuario> Usuarios { get; set; }
+        public UsuarioVM()
+        {
+            this.Usuarios = new List<Usuario>(){
+            new Usuario(){Id= 1, Nombre="Cesar", Apellido="Milstein" },
+            new Usuario(){Id=2, Nombre="Mister", Apellido="X" },
+            new Usuario(){Id=3, Nombre="Tu", Apellido="Vieja" }
+            };
+        }
+
+    }
+}
