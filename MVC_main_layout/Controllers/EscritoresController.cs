49cbab0333035c115c15a81555c0e1ba14d67c1f
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-02-27 14:24:45 -0300

    Added a table that displays multimedia content.

--- MVC_main_layout/Controllers/EscritoresController.cs
+++ MVC_main_layout/Controllers/EscritoresController.cs
@@ -3,18 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MVC_main_layout.Models;
 
 namespace MVC_main_layout.Controllers
 {
     public class EscritoresController : Controller
     {
         [HttpGet]
-        public ActionResult MostrarVendedores()
+        public ActionResult MostrarEscritores()
         {
-
-
-
-            return View();
+            EscritoresVM model = new EscritoresVM();
+            return View(model);
         }
     }
 }
