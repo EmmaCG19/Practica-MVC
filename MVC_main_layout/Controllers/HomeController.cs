21b7757e79dec96c9284dd3bfd5587fd3c990375
Emmanuel Fernández <emmanuelfernandez600@gmail.com>
2020-03-04 21:17:08 -0300

    Revert "Testing HttpGet and queryString with a simple project."
    This reverts commit 714b1f869a2b1c4238a7cd5bd79028886f49faf5.

--- MVC_main_layout/Controllers/HomeController.cs
+++ MVC_main_layout/Controllers/HomeController.cs
@@ -3,42 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using MVC_main_layout.Models;
 
 namespace MVC_main_layout.Controllers
 {
     public class HomeController : Controller
     {
-
-        //Cargar un form con los datos de un usuario y mostrar los mismos en un mensaje utilizando el HttpGet
-
-        [HttpGet]
-        public ActionResult Index(string Id)
+        // GET: Home
+        public ActionResult Index()
         {
-            IndexVM model = new IndexVM();
-            ActionResult action = View(model);
-
-             if (Id != null)
-            {
-                //Busco en los clientes que tengo aquel con ese id
-                //model.ClienteSeleccionado = model.Clientes.Where(c => c.Nombre == nombre).First();
-                try
-                {
-                    model.ClienteSeleccionado = model.Clientes.Where(c => c.Id == Convert.ToInt32(Id)).First();
-
-                    model.Id = model.ClienteSeleccionado.Id;
-                    model.Nombre = model.ClienteSeleccionado.Nombre;
-                    model.Apellido = model.ClienteSeleccionado.Apellido;
-
-                }
-                catch (Exception e)
-                {
-                    action = Content(String.Format("No se pudo encontrar un cliente con esa Id.{0}", e.Message));
-
-                }
-            }
-
-            return action;
+            return View();
         }
     }
 }
--- MVC_main_layout/Models/IndexVM.cs
+++ MVC_main_layout/Models/IndexVM.cs
@@ -7,32 +7,7 @@ namespace MVC_main_layout.Models
 {
     public class IndexVM
     {
-        public IndexVM()
-        {
-            this.Clientes = new List<Cliente>()
-            {
-                new Cliente(){Id= 1, Nombre="Juan", Apellido="Estevez", TieneDeuda=false},
-                new Cliente(){Id= 2, Nombre="Maria", Apellido="Zorragrieta", TieneDeuda=true},
-                new Cliente(){Id= 3, Nombre="Marcelo", Apellido="Gallardo", TieneDeuda=false},
-            };
 
-        }
 
-        public Cliente ClienteSeleccionado{ get; set; }
-        public List<Cliente> Clientes { get; set; }
-
-        //Campos del Form
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public string Apellido { get; set; }
-
-        //Metodos privados
-        private static int GenerarId()
-        {
-            Random rnd = new Random();
-            int id = rnd.Next(1, 100);
-
-            return id;
-        }
     }
 }
